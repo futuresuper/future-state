821dbd38f08ac5a27f7ec0bc4f87fd0791822861
Andrew Sellen <andrewmsellen@gmail.com>
2022-10-09 12:07:24 +1100

    feat: article content styles

--- src/components/MixedTypographyParagraph.svelte
+++ src/components/MixedTypographyParagraph.svelte
@@ -2,10 +2,23 @@
   import TextContent from "./TextContent.svelte";
 
   export let array;
+
+  console.log(array);
 </script>
 
 {#each array as textContent}
-  <TextContent {textContent}>
-    {textContent.value}
-  </TextContent>
+  {#if textContent.nodeType === "hyperlink"}
+    <a href={textContent.data.uri}>
+      {#each textContent.content as nestedTextContent}
+        <!-- <TextContent {nestedTextContent}>
+          {nestedTextContent.value}
+        </TextContent> -->
+        hello
+      {/each}
+    </a>
+  {:else}
+    <TextContent {textContent}>
+      {textContent.value}
+    </TextContent>
+  {/if}
 {/each}
